720cc6162c2b7bf4fc82bab0a630a4e85a23eca1
masaki kusayama <masaki.kusayama@gmail.com>
2020-01-01 21:41:00 +0900

    Add emotion & reset-css

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,9 +1,18 @@
 import React, { FC } from "react";
+import { css } from "@emotion/core";
 
 interface Props {
   name: string;
 }
 
-const Hello: FC<Props> = ({ name }) => <div>Hello, {name}</div>;
+const Hello: FC<Props> = ({ name }) => (
+  <div css={styles.root}>Hello, {name}</div>
+);
+
+const styles = {
+  root: css`
+    color: tomato;
+  `
+};
 
 export default Hello;
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "reset-css/reset.css";
 
 import Hello from "./components/hello";
 
